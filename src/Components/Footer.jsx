3ffbb109588d64bdb4517ff593be1ca45b685b6c
RamzGxZ <ranzdaffa32@gmail.com>
2023-04-02 21:29:56 +0700

    penambahan home dan footer

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <footer className="py-3 my-5">
+                <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pembelajaran</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Info</a></li>
+                    <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Admin</a></li>
+                </ul>
+                <p className="text-center text-body-secondary">© 2023 Elearning anak-TK</p>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Footer from "./Footer";
 
 
 const Home = () => {
@@ -49,7 +50,65 @@ const Home = () => {
                 </div>
             </div>
 
+            <div className="icon w-100 bg-dark d-flex justify-content-center align-items-center mt-5 mb-5" style={{
+                height: '10vh'
+            }}>
+                <div className="i1 d-flex justify-content-center align-items-center" style={{
+                    width: '25%'
+                }}>
+                    <i className="fa-brands fa-youtube text-white" style={{
+                        fontSize: 60
+                    }} />
+                </div>
+                <div className="i2 d-flex justify-content-center align-items-center" style={{
+                    width: '25%'
+                }}>
+                    <i className="fa-solid fa-image text-white" style={{
+                        fontSize: 60
+                    }} />
+                </div>
+                <div className="i3 d-flex justify-content-center align-items-center" style={{
+                    width: '25%'
+                }}>
+                    <i className="fa-solid fa-circle-info text-white" style={{
+                        fontSize: 60
+                    }} />
+                </div>
+            </div>
 
+            <div className="mb-5">
+                <div className="d-flex w-100 container h-100 gap-5">
+                    <div className="vid1 w-50">
+                        <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/JKH_iUiMWSk" title="Anak Paud Belajar di Kelas PG dan TK ✿ Daily Playgroup School Learning Indonesia - Hana Family" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+                    </div>
+
+                    <div className="vidWrapperSamping w-50 flex-column">
+                        <div className="w-100 d-flex gap-5 mb-3">
+                            <div className="vid2 w-50">
+                                <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/83BDo8-1hQs" title="Senam Sehat Gembira, Paud Anggrek." frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+                            </div>
+
+                            <div className="vid3 w-50">
+                                <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/3OP0Qa2agtM" title="DiAJAKIN PAPS AQILLA BERMAIN SEPUASNYA. WAJIB KESINI KALO MAIN KE PINRANG" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+                            </div>
+                        </div>
+
+                        <div className=" w-100 d-flex gap-5 mt-3">
+                            <div className="vid4 w-50">
+                                <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/3OP0Qa2agtM" title="DiAJAKIN PAPS AQILLA BERMAIN SEPUASNYA. WAJIB KESINI KALO MAIN KE PINRANG" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen /> 
+                            </div>
+
+                            <div className="vid5 w-50">
+                                <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/3OP0Qa2agtM" title="DiAJAKIN PAPS AQILLA BERMAIN SEPUASNYA. WAJIB KESINI KALO MAIN KE PINRANG" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen />
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+
+            <Footer/>
         </div>
     )
 }
