10ad33fe7e7cd5ddead11db08629da5e4e4953fa
hildaRX23 <hildaajah426@gmail.com>
2023-04-03 13:30:48 +0700

    edit footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     return (
         <div>
             <footer className="py-3 my-5">
+                <h1>hi</h1>
                 <ul className="nav justify-content-center border-bottom pb-3 mb-3">
                     <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Home</a></li>
                     <li className="nav-item"><a href="#" className="nav-link px-2 text-body-secondary">Pembelajaran</a></li>
