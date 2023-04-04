4e4c066614b8f6fd31e9582f009c1129ed9532dc
dewiAnjani11 <da0189772@.gmail.com>
2023-04-04 20:59:38 +0700

    penambahan import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Home from "./Components/Home"
 import Pembelajaran from "./Components/Pembelajaran"
 import AdminDasboard from "./Components/AdminDasboard"
+import AdminUpload from "./Components/AdminUpload"
 
 
 function App() {
