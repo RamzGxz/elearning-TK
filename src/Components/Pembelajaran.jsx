3f2833772038055ecbb3b06cf3cd7668926256c6
dewiAnjani11 <da0189772@.gmail.com>
2023-04-04 15:32:32 +0700

    perubahan lagi

--- src/Components/Pembelajaran.jsx
+++ src/Components/Pembelajaran.jsx
@@ -1,5 +1,5 @@
 import Header from "./Header";
-
+import Footer from "./Footer";
 const Pembelajaran = () =>{
     return(
         
@@ -9,11 +9,11 @@ const Pembelajaran = () =>{
               <div className="pg_pembelajaran justify-content-center align-items-center ">
                  <h1 className="text-center m-3">video Pembelajaran</h1>
                 <div className="history">
-                    <div className="h2 d-flex justify-content-center gap-5">
-                        <h2>History</h2>
+                    <div className="d-flex justify-content-between m-3">
+                        <h2>History </h2>
                         <h2>more</h2>
                     </div>
-                    <div className="d-flex justify-content-center align-content-center">
+                    <div className="d-flex row row-cols-3 ">
                         <div className="gap-5">
                             <div className="histor  gap-4 m-3">
                             <iframe width={'100%'} height={'100%'}src="https://www.youtube.com/embed/rbIT554Hc7w" title="Omar &amp; Hana | Koleksi Lagu-Lagu Omar &amp; Hana | Tolong Mama Papa &amp; Lain-Lain" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
@@ -31,6 +31,32 @@ const Pembelajaran = () =>{
                         </div>
                     </div>
                 </div>
+                <div className="vpopuler">
+                    <div className="d-flex justify-content-between m-3">
+                        <h2>vidio populer </h2>
+                        <h2>more</h2>
+                    </div>
+                    <div className="d-flex row row-cols-3 ">
+                        <div className="gap-5">
+                            <div className="histor  gap-4 m-3">
+                            <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/U_cc8aCkEwk" title="Video Pembelajaran Tema Tanaman - Sub Tema Tanaman Buah untuk PAUD / TK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div className="gap-5">
+                            <div className="histor  gap-4 m-3">
+                            <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/U_cc8aCkEwk" title="Video Pembelajaran Tema Tanaman - Sub Tema Tanaman Buah untuk PAUD / TK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                        <div className="gap-5">
+                            <div className="histor  gap-4 m-3">
+                            <iframe width={'100%'} height={'100%'} src="https://www.youtube.com/embed/U_cc8aCkEwk" title="Video Pembelajaran Tema Tanaman - Sub Tema Tanaman Buah untuk PAUD / TK" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="footer">
+                <Footer/>
             </div>
      </div>   
     )
