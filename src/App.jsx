510a3ec325685b2f387e5bcdfc217cd8bb3c780f
hildaRX23 <hildaajah426@gmail.com>
2023-04-05 16:36:06 +0700

    Page login

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import Pembelajaran from "./Components/Pembelajaran"
 import AdminDasboard from "./Components/AdminDasboard"
 import AdminUpload from "./Components/AdminUpload"
 import AdminEditProfile from "./Components/AdminEditProfile"
+import PageLogin from "./Components/PageLogin"
 
 function App() {
 
   return (
     <div>
-      <AdminEditProfile/>
+      <PageLogin/>
     </div>
   )
 }
--- src/Components/PageLogin.jsx
+++ src/Components/PageLogin.jsx
@@ -3,40 +3,41 @@ const PageLogin = () => {
         <div>
             <div className="container-fluid">
                 <div className="row">
-                    <div className="col-sm-6 quote-section">
-                        <blockquote className="blockquote">
-                            <p className="blockquote-text">Faith is not belief without proof, but trust without reservation</p>
-                            <footer className="blockquote-footer">D. Elton Trueblood</footer>
+                    <div className="col-sm-6 quotes d-flex align-items-center justify-content-center" style={{
+                        width: '50%'
+                    }}>
+                        <blockquote className="w-50 ">
+                            <p className="fs-5 fw-bolder">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo cum fugiat odio dolor delectus ab illo ad quis rem quidem!</p>
+                            <p className="fs-5">- Yoona Savirah Kholid</p>
                         </blockquote>
                     </div>
-                    <div className="col-sm-6 form-section">
-                        <div className="login-wrapper text-center">
-                            <img src="assets/images/profile-img.jpg" alt className="login-img" />
-                            <form action="#!">
-                                <div className="form-group">
-                                    <label htmlFor="email" className="sr-only">Email</label>
-                                    <input type="email" name="email" id="email" className="form-control" placeholder="Email address" fdprocessedid="4ut9mu" />
-                                </div>
-                                <div className="form-group mb-3">
-                                    <label htmlFor="password" className="sr-only">Password</label>
-                                    <input type="password" name="password" id="password" className="form-control" placeholder="*************" fdprocessedid="2aolnm" />
-                                </div>
-                                <input name="login" id="login" className="btn btn-block login-btn" type="button" defaultValue="LOGIN" fdprocessedid="lrgkcm" />
-                            </form>
-                            <a href="#!" className="forgot-password-link">Forgot Your Password?</a>
-                            <p className="login-wrapper-footer-text">Need an account? <a href="#!" className="text-reset">Signup here</a></p>
+                    <div className="col-sm-6 loginForm d-flex flex-column py-3 justify-content-center align-items-center" style={{
+                        width: '50%',
+                        height: '100vh'
+                    }}>
+                        <i className="fas fa-cubes fa-2x mb-4" style={{ color: '#ff6219', fontSize: 100 }} />
+                        <form action="" className=" w-50">
+                            <input type="Text" placeholder="Username" className="form-control py-3 mb-4 px-4" style={{
+                                borderRadius: '30px'
+                            }} />
+                            <input type="password" placeholder="Password" className="form-control py-3 mb-4 px-4" style={{
+                                borderRadius: '30px'
+                            }} />
+                            <button className="btn btn-dark w-100 fs-3 fw-bolder" style={{
+                                borderRadius: '30px'
+                            }}>LOGIN</button>
+                            <p className="fs-6 text-black-50 text-center mt-4">Lupa Password? <a href="">Hubungi Developer</a></p>
+                        </form>
+
+                        <div style={{
+                            marginTop: '20%'
+                        }}>
+                            © 2020 Copyright:
+                            <a className=" text-black text-decoration-underline mx-2" href="">Yoona</a>
                         </div>
-                        <footer>
-                            <nav className="footer-nav">
-                                <a href="#!">Terms of use.</a>
-                                <a href="#!">Privacy policy</a>
-                            </nav>
-                        </footer>
                     </div>
                 </div>
             </div>
-
-
         </div>
     )
 }
