80e5371811a5e89d83dbdb46544248c2d463baa9
frommetoyou1521 <spensagahrinad@gmail.com>
2023-04-07 21:42:26 +0700

    tambah page info

---
+++ frontend/src/Components/Info.jsx
@@ -0,0 +1,22 @@
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Info = () => {
+    return (
+
+        <div>
+            <Header act3="active" />
+
+            <div class="atas">
+                <h1>INFORMASI</h1>
+                <div>
+                    <p></p>
+                </div>
+            </div>
+            
+
+            <Footer/>
+        </div>
+    )
+}
+export default Info
