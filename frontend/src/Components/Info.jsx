347aaa98e703d4e31e21464ed8741214aad07673
frommetoyou1521 <spensagahrinad@gmail.com>
2023-04-09 02:33:32 +0700

    eidt isi page info

--- frontend/src/Components/Info.jsx
+++ frontend/src/Components/Info.jsx
@@ -9,35 +9,38 @@ const Info = () => {
         <div>
             <Header act3="active" />
 
-            <div className="atas">
-                <h1>INFORMASI</h1>
+            <div className="justify-content-center align-items-center container wrapperInfo bg-blue" style={{marginTop: '10%'}}>
+                <h1 className="text-center pb-5">INFORMASI</h1>
                 <div>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium enim in dolores eius officiis omnis est commodi optio quo deserunt inventore id harum adipisci eos similique minima quia nemo repellendus cum, sint consequuntur et rem fugiat! Nostrum, doloremque minima commodi a optio sint provident culpa alias, soluta cupiditate saepe modi. Corrupti officia aspernatur quae assumenda! Vel nisi nobis vero alias adipisci corrupti repellendus minima sed obcaecati distinctio in consequuntur architecto debitis, quod esse unde ut ipsum laborum! Sed consequuntur neque reprehenderit culpa accusantium id eaque officiis, quo non illo sint omnis voluptas! Labore corrupti corporis reprehenderit vitae vel quia mollitia!</p>
                 </div>
             </div>
-            <section>
-                <h3>Galeri</h3>
-                <div className="galeri">
-                    <div>
-                        <img src="https://unsplash.com/photos/78Ae6N7rNvI" alt="Runad Kelas" />
-                    </div>
-                    <div>
-                        <img src="https://unsplash.com/photos/OVDtgUhUPBY" alt="Balok Angka dan Huruf" />
-                    </div>
-                    <div>
-                        <img src="https://unsplash.com/photos/78Ae6N7rNvI" alt="Anak - Anak Sedang Rekreasi" />
-                    </div>
-                    <div>
-                        <img src="https://unsplash.com/photos/_fTNVaFbMdo" alt="Anak - Anak Sedang Melakukan Kegiatan Menggambar" />
-                    </div>
-                </div>
-            </section>
 
-            <aside>
-                <h3>Kalender</h3>
-                {/* berdasarkan tutor gugel */}
-                <Calendar />
-            </aside>
+            <div className="d-flex flex-row justify-content-center align-items-center container wrapperKegiatan" style={{marginTop: '5%'}}>
+                <section className="w-70 pr-10">
+                    <h3 className="text-center p-3">Galeri</h3>
+                    <div className="galeri">
+                        <div className="p-3">
+                            <img src="https://unsplash.com/photos/78Ae6N7rNvI" className="img-thumbnail" alt="Ruang Kelas" />
+                        </div>
+                        <div className="p-3">
+                            <img src="https://unsplash.com/photos/OVDtgUhUPBY" className="img-thumbnail" alt="Balok Angka dan Huruf" />
+                        </div>
+                        <div className="p-3">
+                            <img src="https://unsplash.com/photos/78Ae6N7rNvI" className="img-thumbnail" alt="Anak - Anak Sedang Rekreasi" />
+                        </div>
+                        <div className="p-3">
+                            <img src="https://unsplash.com/photos/_fTNVaFbMdo" className="img-thumbnail" alt="Anak - Anak Sedang Melakukan Kegiatan Menggambar" />
+                        </div>
+                    </div>
+                </section>
+
+                <aside className="w-25">
+                    <h3 className="text-center pb-3">Kalender</h3>
+                    {/* berdasarkan tutor gugel */}
+                    <Calendar className="justify-content-center align-items-center" />
+                </aside>
+            </div>
 
             <Footer/>
         </div>
