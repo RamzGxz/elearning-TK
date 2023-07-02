a465f3452ba96725f9e75969495a3572a02173a3
dewiAnjani11 <da0189772@.gmail.com>
2023-07-02 22:25:35 +0700

    penghapusan head

--- backend/routes/routeGambar.js
+++ backend/routes/routeGambar.js
@@ -9,11 +9,8 @@ routerGambar.put('/updateGambar/:_id', gambarController.updateGambar)
 routerGambar.post('/postDataGambar', gambarController.postDataGambar)
 routerGambar.post('/postGambarDetail/:_id', gambarController.insertdetail)
 routerGambar.put('/updateGambarDetails/:_id', gambarController.updateDetail)
-<<<<<<< HEAD
 routerGambar.delete('/deleteGambarDetail/:_id',gambarController.deleteDetail)
 routerGambar.get('/getdetailbyid/:_id',gambarController.getDetailById)
-=======
 routerGambar.delete('/deleteGambarDetail/:_id', gambarController.deleteDetail)
->>>>>>> 3bc076a52fa71033c9b1f8c7a4045a46cab1ffcb
 
 module.exports = routerGambar
